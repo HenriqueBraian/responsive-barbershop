1c2df07c532083887438c8c0fcddeb0b33e843ae
unknown <ihbs.0@proton.me>
2026-05-14 12:50:45 -0400

    secao work completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,41 @@
 /*=============== SHOW & CLOSE MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /* Show menu */
-if(navToggle){
-   navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
-   })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /* Hide menu */
-if(navClose){
-   navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu')
-   })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MOBILE MENU ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-   const navMenu = document.getElementById('nav-menu')
-   // When we click on each nav__link, we remove the show-menu class
-   navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE HEADER STYLES ===============*/
-const scrollHeader = () =>{
-   const header = document.getElementById('header')
-   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
-   this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                      : header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER WORKS ===============*/
 const swiperWork = new Swiper('.work__swiper', {
@@ -48,46 +49,33 @@ const swiperWork = new Swiper('.work__swiper', {
     type: 'fraction',
   },
   navigation: {
-    nextEl: ' work__data .swiper-button-next',
-    prevEl: ' work__data .swiper-button-prev',
+    nextEl: '.work__data .swiper-button-next',
+    prevEl: '.work__data .swiper-button-prev',
   },
-  
 });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== GSAP ANIMATION ===============*/ 
-
+/*=============== GSAP ANIMATION ===============*/
 
 /* Home animation */
 
-
 /* Home background animation */
-
 
 /* The nav animation only works in the home section */
 
-
 /* About animation */
-
 
 /* Work animation */
 
-
 /* Service animation */
-
 
 /* Expert animation */
 
-
 /* Contact animation */
-
 
 /* Footer animation */
